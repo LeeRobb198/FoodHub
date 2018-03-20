bc35a72f5c9a28cda3e567379469b6c86ebe9d66
1601432 <l.pl.robbie.ac.uk>
2018-03-20 18:27:35 +0000

    Lee

--- FoodhubFunctionality.js
+++ FoodhubFunctionality.js
@@ -1,24 +1,37 @@
-$(document).ready(function() {
-  // Semantic UI popup
-  $('.js_popup')
-    .popup({
-      on : 'click'
-    });
+$(document).ready(function(){
 
   // Semantic UI Range
-  $('#smooth').range({
+  $('#range-1').range({
     min: 0,
     max: 10,
     start: 5
   });
-
-  $('#price-range').range({
-    min: 0,
-    max: 10,
-    start: 5,
-    labelType: 'letter'
   });
 
+$('#smooth').range({
+   min: 0,
+   max: 10,
+   start: 5,
+   smooth: true
+ });
+
+onChange: function(value) {
+  var
+    $self = $(this),
+    firstVal = $self.range('get thumb value'),
+    secVal = $self.range('get thumb value', 'second');
+  $('#display-d').html('|' + firstVal + " - " + secVal + '| = ' + value);
+}
+});
+
+//Map --------------------------------------------------------------------------
 
 
-  });
+//Popup ------------------------------------------------------------------------
+$('.example .custom.button')
+  .popup({
+    popup : $('.custom.popup'),
+    on    : 'click'
+  })
+;
+>>>>>>> f5985976da0f8ccd64c4c70aa6551d2dfc747672
