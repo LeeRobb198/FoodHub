e30e25b8b72560234b60084f901ed352b3a0cfd8
IivoMitrunen <iivo.mitrunen@gmail.com>
2018-03-20 17:34:15 +0000

    test

--- FoodhubFunctionality.js
+++ FoodhubFunctionality.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
     });
 
   // Semantic UI Range
-  $('#my-range').range({
+  $('#smooth').range({
     min: 0,
     max: 10,
     start: 5
@@ -19,6 +19,6 @@ $(document).ready(function() {
     labelType: 'letter'
   });
 
-  
+
 
   });
