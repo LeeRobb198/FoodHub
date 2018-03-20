dee6fc8a5b447b8c04af042f12a5959896de3535
IivoMitrunen <iivo.mitrunen@gmail.com>
2018-03-20 12:55:53 +0000

    test

---
+++ FoodhubFunctionality.js
@@ -0,0 +1,24 @@
+// All the functionality for html template
+$(document).ready(function() {
+  // Semantic UI popup
+  $('.js_popup')
+    .popup({
+      on : 'click'
+    });
+
+$('#smooth').range({
+   min: 0,
+   max: 10,
+   start: 5,
+   smooth: true
+ });
+});
+
+onChange: function(value) {
+  var
+    $self = $(this),
+    firstVal = $self.range('get thumb value'),
+    secVal = $self.range('get thumb value', 'second');
+  $('#display-d').html('|' + firstVal + " - " + secVal + '| = ' + value);
+}
+});
