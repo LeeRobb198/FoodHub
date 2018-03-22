f6446033aa835f35cf7337e14e45a05d7c919ce4
unknown <iivo.mitrunen@gmail.com>
2018-03-22 08:49:40 +0000

    test

--- FoodhubFunctionality.js
+++ FoodhubFunctionality.js
@@ -1,24 +1,14 @@
 $(document).ready(function(){
-
-
-  // Semantic UI Range
+// Semantic UI Range
   $('.ui.range').range({
     min: 0,
     max: 10,
     start: 5,
-
   });
-
-
-
-$(document).ready(function(){
     // All your normal JS code goes in here
     $(".rating").rating();
-});
-/*$('#toggle').click(function(){
-  $('.ui.sidebar').sidebar('toggle');
-
-});*/
-
+$('body .ui.sidebar').sidebar('setting', 'transitions','overlay')
+  .sidebar('attach events', '.toggle.button')
+  .sidebar('toggle');
 
 });
