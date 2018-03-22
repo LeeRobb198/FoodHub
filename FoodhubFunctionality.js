026c8458a38f8fd2cb0998e2cb9def3128187900
BenH333 <r.hadden@rgu.ac.uk>
2018-03-22 09:54:17 +0000

    Ben

--- FoodhubFunctionality.js
+++ FoodhubFunctionality.js
@@ -8,13 +8,11 @@ $(document).ready(function(){
     // All your normal JS code goes in here
     $(".rating").rating();
 
-<<<<<<< HEAD
-  $('.teal.button')
-    .popup({
-      on: 'click'
-    });
-=======
     $('.sidebar')
       .sidebar('attach events', '.item');
->>>>>>> a49b0a231a83791144a13bb49e5b00389c8a6bb9
+      
+      $('.teal.button')
+        .popup({
+          on: 'click'
+        });
 });
