3466e85d46f54fb0ded9d255c5b49de3d63410aa
Lee Robbie <leerobb198@Lees-MacBook-Pro.local>
2018-04-18 10:54:09 +0100

    Price Option

--- AutoComplete.js
+++ AutoComplete.js
@@ -1,9 +1,9 @@
 function initAutocomplete() {
   // Create the autocomplete object, restricting the search to geographical
   // location types.
-  //autocomplete = new google.maps.places.Autocomplete(
-    ///** @type {!HTMLInputElement} */(document.getElementById('mobileLocation')),
-    //{types: ['geocode']});
+  autocomplete = new google.maps.places.Autocomplete(
+    /** @type {!HTMLInputElement} */(document.getElementById('mobileLocation')),
+    {types: ['geocode']});
 
   autocomplete = new google.maps.places.Autocomplete(
     /** @type {!HTMLInputElement} */(document.getElementById('mainLocation')),
