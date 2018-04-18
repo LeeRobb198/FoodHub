158554901432c38f3772cef7b428f68a5e62682f
BenH333 <r.hadden@rgu.ac.uk>
2018-04-18 11:49:54 +0100

    123

---
+++ public/FoodhubFunctionality.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+// Semantic UI Range
+  $('.ui.range').range({
+    min: 0,
+    max: 10,
+    start: 5,
+  });
+    // All your normal JS code goes in here
+    $(".rating").rating();
+
+    $('.sidebar')
+      .sidebar('attach events', '.item');
+
+
+      $('#showReviews').click(function(){
+          $('#reviews').css({"display": "inline"});
+      });
+});
