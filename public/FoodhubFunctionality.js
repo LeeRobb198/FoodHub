acc1f160ce0bb3f81c098db8f87b441a70b604e5
Reuben Hadden <reubenhadden@MacBook-Air.local>
2018-04-19 11:50:36 +0100

    123

--- public/FoodhubFunctionality.js
+++ public/FoodhubFunctionality.js
@@ -11,9 +11,8 @@ $(document).ready(function(){
     $('.sidebar')
       .sidebar('attach events', '.item');
 
-
       $('#showReviews').click(function(){
           $('#reviews').css({"display": "inline"});
-          $.getJSON("server.js", function(result) { 
+          $.getJSON("server.js", function(result) {
       });
 });
