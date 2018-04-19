62181fd0cf6c14e2ff5581489471d67a76d211ec
Reuben Hadden <reubenhadden@MacBook-Air.local>
2018-04-19 12:00:02 +0100

    123

--- server.js
+++ server.js
@@ -36,54 +36,7 @@ app.get('/all', function(req, res) {
     }
 */
     db.collection('data').find({}).toArray(function(err, result) {
-	if (err) throw err;
+	     if (err) throw err;
         res.json(result);
         });
-});
-
-//Adding Route
-app.post('/addreview', function (req, res) {
-    db.collection('data').save(req.body, function(err, result) {
-      if (err) throw err;
-      console.log('saved to database');
-      res.redirect('/');
-  });
-});
-
-//Filtering Route
-app.post('/search', function(req, res) {
-  db.collection('data').find(req.body).toArray(function(err, result) {
-    if (err) throw err;
-
-    var output ="<h1>All the quotes</h1>";
-
-    for (var i =0; i < result.length; i++) {
-      output += "<div>"
-      output += "<h3>" + result[i].name + "</h3>"
-      output += "<p>" + result[i].date + "</p>"
-      output += "<p>" + result[i].restaurant + "</p>"
-      output += "<p>" + result[i].review + "</p>"
-      output += "<p>" + result[i].rating
-    }
-    res.send(output);
-  });
-});
-
-//Delete Route
-app.post('/delete', function(req, res) {
-    db.collection('data').deleteOne(req.body, function(err, result) {
-      if (err) throw err;
-      res.redirect('/');
-  });
-});
-
-//Update Route
-app.post('/update', function(req, res) {
-  var query = { quote: req.body.quote };
-  var newvalues = { $set: {name: req.body.newname, quote: req.body.newquote } };
-
-  db.collection('reviews').updateOne(query,newvalues, function(err, result) {
-    if (err) throw err;
-    res.redirect('/');
-  });
 });
