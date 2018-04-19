4d5b63c7ca001409d2f95f14f63191367923bd2e
unknown <iivo.mitrunen@gmail.com>
2018-04-19 17:37:53 +0100

    test

--- server.js
+++ server.js
@@ -64,10 +64,10 @@ app.post('/dologin', function(req, res) {
   db.collection('people').findOne({"login.username":uname}, function(err, result) {
     if (err) throw err;//if there is an error, throw the error
     //if there is no result, redirect the user back to the login system as that username must not exist
-    if(!result){res.redirect('pages/FoodHub-Login');return}
+    if(!result){res.redirect('/FoodHub-Login');return}
     //if there is a result then check the password, if the password is correct set session loggedin to true and send the user to the index
     if(result.login.password == pword){ req.session.loggedin = true;  res.redirect('/FoodHub') }
     //otherwise send them back to login
-    else{res.redirect('pages/FoodHub-Login')}
+    else{res.redirect('/FoodHub-Login')}
   });
 });
