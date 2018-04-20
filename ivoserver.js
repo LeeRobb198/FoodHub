0ccd1292059975623bc5e24e5087d107effd671f
Reuben Hadden <reubenhadden@MacBook-Air.local>
2018-04-20 14:39:05 +0100

    ejs redone

---
+++ ivoserver.js
@@ -0,0 +1,78 @@
+const MongoClient = require('mongodb').MongoClient; //npm install mongodb@2.2.32
+const url = "mongodb://localhost:27017/profiles";
+const express = require('express'); //npm install express
+const session = require('express-session'); //npm install express-session
+const bodyParser = require('body-parser'); //npm install body-parser
+const app = express();
+app.use(express.static(__dirname + '/public'));
+app.set('views', __dirname + '/views');
+//this tells express we are using sesssions. These are variables that only belong to one user of the site at a time.
+app.use(session({ secret: 'example' }));
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+var db;
+
+
+//this is our connection to the mongo db, ts sets the variable db as our database
+MongoClient.connect(url, function(err, database) {
+  if (err) throw err;
+  db = database;
+  app.listen(8080);
+  console.log('listening on 8080');
+});
+
+
+//this is our root route
+app.get('/', function(req, res) {
+  //if the user is not logged in redirect them to the login page
+  if(!req.session.loggedin){res.redirect('/FoodHub-Login');return;}
+});
+//this is our login route, all it does is render the login.ejs page
+app.get('/FoodHub-Login', function(req, res) {
+  res.render('pages/FoodHub-Login');
+});
+
+app.get('/Foodhub', function(req, res) {
+  res.render('pages/Foodhub');
+ });
+ app.get('/FoodHub-T&C', function(req, res) {
+   res.render('pages/FoodHub-T&C');
+  });
+
+
+
+ // log out button
+
+/*app.get('/logout', function(req, res) {
+  req.session.loggedin = false;
+  req.session.destroy();
+  res.redirect('/FoodHub-Login.html');
+});*/
+
+
+
+//the dologin route detasl with the data from the login screen.
+//the post variables, username and password ceom from the form on the login page.
+
+app.post('/dologin', function(req, res) {
+  console.log(JSON.stringify(req.body))
+  var uname = req.body.username;
+  var pword = req.body.password;
+
+
+  db.collection('people').findOne({"login.username":uname}, function(err, result) {
+    if (err) throw err;//if there is an error, throw the error
+    //if there is no result, redirect the user back to the login system as that username must not exist
+    if(!result){res.redirect('/FoodHub-Login');return}
+    //if there is a result then check the password, if the password is correct set session loggedin to true and send the user to the index
+    if(result.login.password == pword){ req.session.loggedin = true;  res.redirect('/FoodHub') }
+    //otherwise send them back to login
+    else{res.redirect('/FoodHub-Login')}
+  });
+});
--- server.js
+++ server.js
@@ -1,23 +1,31 @@
 const MongoClient = require('mongodb').MongoClient;
 const url = "mongodb://localhost:27017/review_data";
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
 const path = require("path");
-
+const express = require('express'); //npm install express
+const session = require('express-session'); //npm install express-session
+const bodyParser = require('body-parser'); //npm install body-parser
+const app = express();
+app.use(express.static(__dirname + '/public'));
+app.set('views', __dirname + '/views');
 app.use(bodyParser.urlencoded({extended:true}));
-//app.use(express.static('public'));
+//this tells express we are using sesssions. These are variables that only belong to one user of the site at a time.
+app.use(session({ secret: 'example' }));
 
-//set static path
-//app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static('public', {index: 'Foodhub.html'}))
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
 
 var db;
 
-MongoClient.connect(url, function(err, database){
-  if(err) throw err;
+//this is our connection to the mongo db, ts sets the variable db as our database
+MongoClient.connect(url, function(err, database) {
+  if (err) throw err;
   db = database;
   app.listen(8080);
+  console.log('listening on 8080');
 });
 
 /*
@@ -40,7 +48,25 @@ app.get('/', function(req, res) {
     });
   });
 */
+
+//this is our root route
 app.get('/', function(req, res) {
+  //if the user is not logged in redirect them to the login page
+  if(!req.session.loggedin){res.redirect('/FoodHub');return;}
+});
+//this is our login route, all it does is render the login.ejs page
+app.get('/FoodHub-Login', function(req, res) {
+  res.render('pages/FoodHub-Login');
+});
+
+app.get('/Foodhub', function(req, res) {
+  res.render('pages/Foodhub');
+ });
+ app.get('/FoodHub-T&C', function(req, res) {
+   res.render('pages/FoodHub-T&C');
+  });
+
+app.get('/all', function(req, res) {
 //    if (err) throw err;
 
 /*  var output = "<h1>All the reviews</h1>";
