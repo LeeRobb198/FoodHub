67a81e0dbe521b93a5a7d91d0fc335a4caaddb01
IivoMitrunen <iivo.mitrunen@gmail.com>
2018-04-20 13:30:54 +0100

    test

--- server.js
+++ server.js
@@ -72,7 +72,7 @@ app.post('/dologin', function(req, res) {
     //if there is no result, redirect the user back to the login system as that username must not exist
     if(!result){res.redirect('/FoodHub-Login');return}
     //if there is a result then check the password, if the password is correct set session loggedin to true and send the user to the index
-    if(result.login.password == pword){ req.session.loggedin = true;  res.redirect('/FoodHub') }
+    if(result.login.password == pword){ req.session.loggedin = true;  res.redirect('/FoodHub-T&C') }
     //otherwise send them back to login
     else{res.redirect('/FoodHub-Login')}
   });
