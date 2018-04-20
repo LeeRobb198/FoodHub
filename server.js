0ce531432cc31b98ad647b9c2b1ffd05b1a510a6
Ben Hadden <r.hadden@rgu.ac.uk>
2018-04-20 11:39:20 +0100

    123

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 //set static path
 //app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static('views', {index: 'index.ejs'}))
+//app.use(express.static('views', {index: 'index.ejs'}))
 
 var db;
 
