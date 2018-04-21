4e63eeb0cc166c4b681a9b92507db3e449ef73c7
unknown <iivo.mitrunen@gmail.com>
2018-04-21 12:51:49 +0100

    test

--- server.js
+++ server.js
@@ -96,6 +96,6 @@ var datatostore = {
     if (err) throw err;
     console.log('saved to database')
     //when complete redirect to the index
-    res.redirect('/')
+    res.redirect('/FoodHub-Login')
   })
 });
