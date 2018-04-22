7bc94272f4b74b4918bd94b8f94440e911210702
Ben Hadden <r.hadden@rgu.ac.uk>
2018-04-22 14:51:47 +0100

    123

--- public/js/FoodhubFunctionality.js
+++ public/js/FoodhubFunctionality.js
@@ -17,6 +17,6 @@ $(document).ready(function(){
       });
 
       $('#write').click(function(){
-        window.location.href = "pages/addReview.ejs"
+        window.location.href = "pages/addReview.html"
       });
 });
