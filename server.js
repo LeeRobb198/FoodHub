18a1d686363b60be2ce4a467d9c6894dc23e0b94
Reuben Hadden <reubenhadden@MacBook-Air.local>
2018-04-24 11:36:30 +0100

    123

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
-const MongoClient = require('mongodb').MongoClient;
-const url = "mongodb://localhost:27017/review_data";
-const path = require("path");
+const MongoClient = require('mongodb').MongoClient; //npm install mongodb@2.2.32
+const url = "mongodb://localhost:27017/profiles";
 const express = require('express'); //npm install express
 const session = require('express-session'); //npm install express-session
 const bodyParser = require('body-parser'); //npm install body-parser
 const app = express();
-
 app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
-//app.use(bodyParser.urlencoded({extended:true}));
 //this tells express we are using sesssions. These are variables that only belong to one user of the site at a time.
 app.use(session({ secret: 'example' }));
 
@@ -20,6 +17,7 @@ app.use(bodyParser.urlencoded({
 app.set('view engine', 'ejs');
 
 var db;
+
 
 //this is our connection to the mongo db, ts sets the variable db as our database
 MongoClient.connect(url, function(err, database) {
@@ -33,13 +31,7 @@ MongoClient.connect(url, function(err, database) {
 //this is our root route
 app.get('/', function(req, res) {
   //if the user is not logged in redirect them to the login page
-  //if(!req.session.loggedin){res.redirect('/FoodHub-Login');return;}
-  db.collection('data').find({}).toArray(function(err, result) {
-    console.log(result);
-    res.render('pages/Foodhub', {
-        users: result
-      });
-  });
+  if(!req.session.loggedin){res.redirect('/FoodHub-Login');return;}
 });
 //this is our login route, all it does is render the login.ejs page
 app.get('/FoodHub-Login', function(req, res) {
@@ -54,32 +46,33 @@ app.get('/Foodhub', function(req, res) {
   });
 
 
-//Test mongodb data
-app.get('/all', function(req, res) {
-    //Gathers review_data from mongodb, then outputs at JSON
-    db.collection('data').find({}).toArray(function(err, result) {
-	     if (err) throw err;
-        res.json(result);
-        });
-});
 
-/*
-app.get('/', function(req, res) {
-  //if the user is not logged in redirect them to the login page
-  //if(!req.session.loggedin){res.redirect('/FoodHub');return;}
-  res.render('pages/Foodhub')
+ // log out button
+
+/*app.get('/logout', function(req, res) {
+  req.session.loggedin = false;
+  req.session.destroy();
+  res.redirect('/FoodHub-Login.html');
+});*/
 
 
- db.collection('data').find({}).toArray(function(err, result) {
-     if (err) throw err;
-      res.render(json(result));
+
+//the dologin route detasl with the data from the login screen.
+//the post variables, username and password ceom from the form on the login page.
+
+app.post('/dologin', function(req, res) {
+  console.log(JSON.stringify(req.body))
+  var uname = req.body.username;
+  var pword = req.body.password;
+
+
+  db.collection('people').findOne({"login.username":uname}, function(err, result) {
+    if (err) throw err;//if there is an error, throw the error
+    //if there is no result, redirect the user back to the login system as that username must not exist
+    if(!result){res.redirect('/FoodHub-Login');return}
+    //if there is a result then check the password, if the password is correct set session loggedin to true and send the user to the index
+    if(result.login.password == pword){ req.session.loggedin = true;  res.redirect('/FoodHub') }
+    //otherwise send them back to login
+    else{res.redirect('/FoodHub-Login')}
   });
-  /*
-      db.data.find(function(err, docs){
-        console.log("working");
-        res.render('pages/Foodhub',{
-
-        })
-*/
-
-//});
+});
