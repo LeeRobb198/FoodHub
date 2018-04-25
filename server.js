d35ea732e8bf74f3b034089c87d602519e57a45d
Reuben Hadden <reubenhadden@MacBook-Air.local>
2018-04-25 12:14:56 +0100

    123

--- server.js
+++ server.js
@@ -73,7 +73,7 @@ app.post('/dologin', function(req, res) {
   var pword = req.body.password;
 
 
-  db.collection('profiles').findOne({"login.username":uname}, function(err, result) {
+  db.collection('profiles').findOne({"login.user":uname}, function(err, result) {
     if (err) throw err;//if there is an error, throw the error
     //if there is no result, redirect the user back to the login system as that username must not exist
     if(!result){res.redirect('/FoodHub-Login');return}
