be3c564463b4cf344ee7dbda1728cc8a4d850001
IivoMitrunen <iivo.mitrunen@gmail.com>
2018-04-26 14:08:47 +0100

    test

--- public/js/AutoComplete.js
+++ public/js/AutoComplete.js
@@ -1,65 +1,21 @@
 function initAutocomplete() {
-  // Create the autocomplete object, restricting the search to geographical
-  // location types.
+  //Mobile AutoComplete --------------------------------------------------------
   autocomplete = new google.maps.places.Autocomplete(
     /** @type {!HTMLInputElement} */(document.getElementById('mobileLocation')),
     {types: ['geocode']});
+  //End Mobile AutoComplete ----------------------------------------------------
 
+  //Main AutoComplete ----------------------------------------------------------
   autocomplete = new google.maps.places.Autocomplete(
     /** @type {!HTMLInputElement} */(document.getElementById('mainLocation')),
     {types: ['geocode']});
+  //End Main AutoComplete ------------------------------------------------------
 
-    //In this case it gets the address from an element on the page, but obviously you  could just pass it to the method instead
-    //var address = document.getElementById( 'mainLocation' ).value;
+  autocomplete.addListener('place_changed', fillInAddress);
 
-    //autocomplete.addListener('place_changed', function() {
-          /*infowindow.close();
-          marker.setVisible(false);
-          var place = autocomplete.getPlace();
-          if (!place.geometry) {
-            // User entered the name of a Place that was not suggested and
-            // pressed the Enter key, or the Place Details request failed.
-            window.alert("No details available for input: '" + place.name + "'");
-            return;*/
+}
 
-            //var geocoder = new google.maps.Geocoder();
-
-            //geocoder.geocode( { 'address' : address }, function( results, status ) {
-                //if( status == google.maps.GeocoderStatus.OK ) {
-
-                    //In this case it creates a marker, but you can get the lat and lng from the location.LatLng
-                  //  map.setCenter( results[0].geometry.location );
-                  //  var marker = new google.maps.Marker( {
-                //        map     : map,
-                //        position: results[0].geometry.location.LatLng
-                //    } );
-              //  } else {
-              //      alert( 'Geocode was not successful for the following reason: ' + status );
-              //  }
-          //  } );
-
-
-
-
-
-            //latLng = address.geometry.location.LatLng
-            //var marker = new google.maps.Marker({
-          // The below line is equivalent to writing:
-          // position: new google.maps.LatLng(-34.397, 150.644)
-          //position: {lat: -34.397, lng: 150.644},
-          //map: map
-            //console.log(latLng);
-            //console.log("changed");
-          //});
-
-
-    // When the user selects an address from the dropdown, populate the address
-    // fields in the form.
-    autocomplete.addListener('place_changed', fillInAddress);
-
-  }
-
-  function fillInAddress() {
-    // Get the place details from the autocomplete object.
-    var place = autocomplete.getPlace();
-  }
+function fillInAddress() {
+  // Get the place details from the autocomplete object.
+  var place = autocomplete.getPlace();
+}
--- public/js/GoogleMap.js
+++ public/js/GoogleMap.js
@@ -9,28 +9,17 @@ var infowindow;
 var request;
 var service;
 var markers = [];
-//var rating = 1;
 
+//Start of initialiseMap Function --------------------------------------------------------
 function initialiseMap() {
 
-  //Chicago
-  //var center = new google.maps.LatLng(41.8781,-87.6298);
-
-  //New York
+  //Sets the Initial center to New York
   var center = new google.maps.LatLng(40.7128,-74.0060);
-  //Aberdeen
-  //var center = new google.maps.LatLng(57.1497,-2.099075);
 
   map = new google.maps.Map(document.getElementById('map'),{
     center: center,
     zoom: 8
   });
-
-  //var center = new google.maps.LatLng(57.1497,-2.099075);
-  //map = new google.maps.Map(document.getElementById('map'),{
-    //center: center,
-    //zoom: 5
-  //});
 
   //Finds Generic Restaurants
   request = {
@@ -38,89 +27,118 @@ function initialiseMap() {
     radius: 8047,
     types: ['restaurant'],
   };
-  infowindow = new google.maps.InfoWindow();
 
+  //Finds the restaurant's names
+  infowindow = new google.maps.InfoWindow();
   service = new google.maps.places.PlacesService(map);
 
   service.nearbySearch(request, callback);
 
-  //Once Submit Button Hit -------------------------------------------------
-  var button = document.getElementById('subButton');
+  //Once Submit Button Hit -------------------------------------------
+  var isDesktop = true;
+  var mobileButton = document.getElementById('subButtonMobile');
+  var desktopButton = document.getElementById('subButton');
 
   //Geocoder
   var geocoder = new google.maps.Geocoder();
 
-  google.maps.event.addDomListener(button, 'click', function() {
-
-    //Creates New Center
-    var newCenter = new google.maps.LatLng(40.7128,-74.0060);
+  google.maps.event.addDomListener(desktopButton, 'click', function() {
+    isDesktop = true;
     clearResults(markers);
+    geocodeAddress(geocoder,updateMap, isDesktop);
+  });
 
-    //Location Option
-    //var newCenter = new google.maps.LatLng(geocodeAddress(geocoder, map));
-    //console.log(newCenter);
+  google.maps.event.addDomListener(mobileButton, 'click', function() {
+    isDesktop = false;
+    clearResults(markers);
+    geocodeAddress(geocoder,updateMap, isDesktop);
+  });
+  ////End of Submit Button Hit ---------------------------------------
 
-    //Distance Option
-    var distance = document.getElementById('distanceOption');
-    var userDistanceValue = distance.value;
+}
+//End of initialiseMap Function ----------------------------------------------------------
 
-    //Cuisine Option
-    var cuisine = document.getElementById('cuisineOption');
-    var userCuisineOption = cuisine.options[cuisine.selectedIndex].text;
+//Start of updateMap ---------------------------------------------------------------------
+function updateMap(center, isDesktop){
 
-    //Rating Option
-    // $('.ui.rating').rating('setting', 'onRate', function(value) {
-    //   rating = value;
-    // });
-    // console.log("outside: " + rating);
-    // var userRatingOption = rating;
-    // console.log(userRatingOption);
+  map = new google.maps.Map(document.getElementById('map'),{
+    center: center,
+    zoom: 8
+  });
 
-    //Price Option
-    var price = document.getElementsByName('priceOption');
+  //Distance Option
+  var distance;
+  var userDistanceValue;
 
-    for (var i = 0, length = price.length; i < length; i++){
-     if (price[i].checked){
+  if (!isDesktop){
+    distance = document.getElementById('distanceOptionMobile');
+    userDistanceValue = distance.value;
+  }
+  else{
+    distance = document.getElementById('distanceOption');
+    userDistanceValue = distance.value;
+  }
+
+  //Cuisine Option
+  var cuisine;
+  var userCuisineOption;
+
+  if (!isDesktop){
+    cuisine = document.getElementById('cuisineOptionMobile');
+    userCuisineOption = cuisine.options[cuisine.selectedIndex].text;
+  }
+  else{
+    cuisine = document.getElementById('cuisineOption');
+    userCuisineOption = cuisine.options[cuisine.selectedIndex].text;
+  }
+
+  //Price Option
+  var price;
+
+  if (!isDesktop){
+    price = document.getElementsByName('priceOptionMobile');
+  }
+  else{
+    price = document.getElementsByName('priceOption');
+  }
+
+  for (var i = 0, length = price.length; i < length; i++){
+    if (price[i].checked){
       var userPriceOption = price[i].value;
       var minPrice = userPriceOption - 1;
       break;
-     }
     }
+  }
 
-    //Search Request
-    var request = {
-      location: newCenter,
-      radius: userDistanceValue,
-      types: ['restaurant'],
-      keyword: [userCuisineOption],
-      minPriceLevel: minPrice,
-      maxPriceLevel: userPriceOption,
-      //rating: userRatingOption
-    };
-    service.nearbySearch(request, callback);
-  })
-
+  //Search Request
+  var request = {
+    location: center,
+    radius: userDistanceValue,
+    types: ['restaurant'],
+    keyword: [userCuisineOption],
+    minPriceLevel: minPrice,
+    maxPriceLevel: userPriceOption,
+  };
+  service.nearbySearch(request, callback);
 }
+//End of updateMap Function --------------------------------------------------------------
 
-//End of initialiseMap Function --------------------------------------------
-
-//CallBack Function
+//CallBack Function ------------------------------------------------------------
 function callback(results, status){
   if(status == google.maps.places.PlacesServiceStatus.OK){
     for (var i = 0; i < results.length; i++){
-      //createMarker(results[i]);
       markers.push(createMarker(results[i]));
     }
   }
 }
+//End of CallBack Function -----------------------------------------------------
 
-//Creates Marker
+//Creates Marker Function ------------------------------------------------------
 function createMarker(place){
   var placeLoc = place.geometry.location;
   var marker = new google.maps.Marker({
     map: map,
     position: place.geometry.location
-
   });
 
   google.maps.event.addListener(marker, 'click', function(){
@@ -130,153 +148,56 @@ function createMarker(place){
   });
   return marker;
 }
+//End Create Marker Function ---------------------------------------------------
 
-//Clears the Results
+//Clear Results Function -------------------------------------------------------
 function clearResults(markers){
   for (var m in markers){
     markers[m].setMap(null)
   }
   markers = []
 }
+//Clear Results Function -------------------------------------------------------
 
-//Geocoder Function
-function geocodeAddress(geocoder, resultsMap) {
-    var address = document.getElementById('mainLocation').value;
-    var latitude;
-    var longitude;
+//Geocoder Function ------------------------------------------------------------
+function geocodeAddress(geocoder,domapstuff, isDesktop) {
+  var address;
+  var latitude;
+  var longitude;
+
+  if (!isDesktop){
+    var address = document.getElementById('mobileLocation').value;
 
     geocoder.geocode( { 'address': address}, function(results, status) {
       if (status == google.maps.GeocoderStatus.OK) {
         latitude = results[0].geometry.location.lat();
         longitude = results[0].geometry.location.lng();
-        console.log(latitude);
-        console.log(longitude);
+
+        var newCenter = new google.maps.LatLng(latitude,longitude);
+        updateMap(newCenter,isDesktop);
+      }
+      else{
+        var falseCenter = new google.maps.LatLng(40.7128,-74.0060);
+        updateMap(falseCenter, isDesktop);
       }
     });
-    var newCenter = new google.maps.LatLng(latitude,longitude);
-    console.log(newCenter)
-    return newCenter;
   }
+  else{
+      var address = document.getElementById('mainLocation').value;
 
-    //geocoder.geocode({'address': address}, function(results, status) {
-      //if (status === 'OK') {
-      //  resultsMap.setCenter(results[0].geometry.location);
-      //  var latitude = results[0].geometry.location.latitude;
-      //  var longitude = results[0].geometry.location.longitude;
-      //  var marker = new google.maps.Marker({
-      //    map: resultsMap,
-      //    position: results[0].geometry.location
-      //  });
-    //  } else {
-    //    alert('Geocode was not successful for the following reason: ' + status);
-    //  }
-  //  });
-//  }
+      geocoder.geocode( { 'address': address}, function(results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+          latitude = results[0].geometry.location.lat();
+          longitude = results[0].geometry.location.lng();
 
-google.maps.event.addDomListener(window, 'load', initialiseMap);
-
-
-
-//------------------------------------------------------------------------------
-
-
-//Star Rating by John
-
-
-// function initialiseMap() {
-//
-//   //var start = document.getElementById("mainLocation").value;
-//   var center = new google.maps.LatLng(57.1497,-2.099075);
-//   map = new google.maps.Map(document.getElementById('map'),{
-//     center: center,
-//     zoom: 5
-//   });
-//
-//   //var center = new google.maps.LatLng(57.1497,-2.099075);
-//   //map = new google.maps.Map(document.getElementById('map'),{
-//     //center: center,
-//     //zoom: 5
-//   //});
-//
-//   //Finds restaurants
-//   request = {
-//     location: center,
-//     radius: 8047,
-//     types: ['restaurant'],
-//   };
-//   infowindow = new google.maps.InfoWindow();
-//
-//   service = new google.maps.places.PlacesService(map);
-//
-//   service.nearbySearch(request, callback);
-//
-//   //Once Submit Button Hit
-//   var button = document.getElementById('subButton');
-//
-//   google.maps.event.addDomListener(button, 'click', function() {
-//     var newCenter = new google.maps.LatLng(57.1497,-2.099075);
-//     clearResults(markers);
-//
-//     //Cuisine Option
-//     var cuisine = document.getElementById("cuisineOption");
-//     var userCuisineOption = cuisine.options[cuisine.selectedIndex].text
-//
-//     //Price Option
-//     //var ratingValue;
-//
-//     //Search Request
-//     var request = {
-//       location: newCenter,
-//       radius: 8047,
-//       types: ['restaurant'],
-//       keyword: [userCuisineOption],
-//       //rating: [ratingValue]
-//     };
-//     service.nearbySearch(request, callback);
-//   })
-//
-// }
-//
-// function callback(results, status){
-//   if(status == google.maps.places.PlacesServiceStatus.OK){
-//     for (var i = 0; i < results.length; i++){
-//       var m = createMarker(results[i]);
-//       markers.push(m);
-//       //if (m){
-//       //markers.push(m);
-//       //}
-//     }
-//   }
-// }
-//
-// function createMarker(place){
-//   //if (place.rating >= ratingValue){
-//
-//     var placeLoc = place.geometry.location;
-//     var marker = new google.maps.Marker({
-//       map: map,
-//       position: place.geometry.location
-//     });
-//
-//     google.maps.event.addListener(marker, 'click', function(){
-//       infowindow.setContent(place.name);
-//       infowindow.open(map, this);
-//     });
-//     return marker;
-//   //}
-//   //return null;
-// }
-//
-// function clearResults(markers){
-//   for (var m in markers){
-//     markers[m].setMap(null)
-//   }
-//   markers = []
-// }
-//
-// google.maps.event.addDomListener(window, 'load', initialiseMap);
-//
-// </script> -->
-//
-// <!-- Script for Google Map API -->
-// <!--<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDKmr5cGY2-bu4wHPpMSxsvrpH5XM6-Cps&callback=myMap"></script> -->
+          var newCenter = new google.maps.LatLng(latitude,longitude);
+          updateMap(newCenter, isDesktop);
+        }
+        else{
+          var falseCenter = new google.maps.LatLng(40.7128,-74.0060);
+          updateMap(falseCenter, isDesktop);
+        }
+      });
+    }
+}
+//End of Geocoder Function -----------------------------------------------------
